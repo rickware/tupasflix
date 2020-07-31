e46ceb47d8357af4cbfe815393c6c5d155095c25
rickware <ricardo.c.rod@outlook.com>
2020-07-31 09:57:51 -0300

    Migrating to JSX
    Renamed js files and reiniting package-json
    Renamed images files Logo Tupas

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,9 +6,9 @@ function Footer() {
     <FooterBase>
       <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      <p>
-        Imersão React
-      </p>
+			<p>
+			  Imersão React
+			</p>
       </a>
     </FooterBase>
   );
